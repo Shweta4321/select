e671b199f5d5bb012e1868cc0684ee170e012873
Shweta Kumari <201552042@iiitvadodara.ac.in>
2019-12-27 14:38:07 +0530

    Testing

--- select.js
+++ select.js
@@ -2,7 +2,7 @@ window.addEventListener('message', function(eventData) {
     try {
         if (JSON.parse(eventData.data)) {
             let event = JSON.parse(eventData.data);
-            console.log(event.data.data,"in event)
+            console.log(event.data.data,"in event")
             if (event.event_code === "custom-event" && event.data && event.data.code === "payment-event") {
                 var pageContent = '<html><head></head><body><form id="paymentForm" action="' + event.data.data.link + '" method="post">' + '<input type="hidden" name="lan" value="' + event.data.data.lan + '">' + '<input type="hidden" name="amount" value="' + event.data.data.amount + '">' + '<input type="hidden" name="email" value="' + event.data.data.email + '">' + '<input type="hidden" name="mobile" value="' + event.data.data.mobile + '">' + '<input type="hidden" name="wish" value="' + event.data.data.wish + '">' + '<input type="hidden" name="token" value="' + event.data.data.token + '">' + '<input type="hidden" name="allLans" value="' + event.data.data.lan + '">' + '<input type="hidden" name="flag" value="' + event.data.data.flag + '">' + '</form> <script type="text/javascript">document.getElementById("paymentForm").submit();</script></body></html>';
                 var newWindow = window.open();
