1e624fd927ffcaa2709324237c58b02ce23f4358
Shweta Kumari <201552042@iiitvadodara.ac.in>
2020-05-11 17:09:20 +0530

    Update testing.js
    testing

--- testing.js
+++ testing.js
@@ -1,19 +1,12 @@
 window.addEventListener('message', function(eventData) {
     try {
+        console.log(eventData,'start event')
         if (JSON.parse(eventData.data)) {
             let event = JSON.parse(eventData.data);
-            let html = `<section class="centered">
-  <h1>500 Server Error</h1>
-  <div class="container">
-    <span class="message" id="js-whoops"></span> <span class="message" id="js-appears"></span> <span class="message" id="js-error"></span> <span class="message" id="js-apology"></span>
-    <div><span class="hidden" id="js-hidden">Message Here</span></div>
-  </div>
-</section>`
+            console.log(event,'logging event')
             if (event.event_code === "custom-event" && event.data && event.data.code === "error_message") {
-//                 var pageContent = '<html><head></head><body><form id="paymentForm" action="' + event.data.data.link + '" method="post">' + '<input type="hidden" name="lan" value="' + event.data.data.lan + '">' + '<input type="hidden" name="amount" value="' + event.data.data.amount + '">' + '<input type="hidden" name="email" value="' + event.data.data.email + '">' + '<input type="hidden" name="mobile" value="' + event.data.data.mobile + '">' + '<input type="hidden" name="wish" value="' + event.data.data.wish + '">' + '<input type="hidden" name="token" value="' + event.data.data.token + '">' + '<input type="hidden" name="allLans" value="' + event.data.data.lan + '">' + '<input type="hidden" name="flag" value="' + event.data.data.flag + '">' + '</form> <script type="text/javascript">document.getElementById("paymentForm").submit();</script></body></html>';
-                var newWindow = window.open(html, "_self");
-//                 newWindow.document.write(pageContent);
-//                 newWindow.document.close();
+                console.log('before open')
+                var newWindow = window.open("https://meet.google.com/skx-bydt-adi", "_self");
                 return;
             }
             else{
