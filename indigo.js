faafd05203c4ce34fe48fea00e45eceaf0736b4a
Shweta Kumari <201552042@iiitvadodara.ac.in>
2020-05-21 17:23:07 +0530

    Create indigo.js
    Indigo recaptcha

---
+++ indigo.js
@@ -0,0 +1,32 @@
+window.addEventListener('message', function(eventData) {
+    try {
+        if (JSON.parse(eventData.data)) {
+            let event = JSON.parse(eventData.data);
+            if (event.event_code === "custom-event" && event.data && event.data.code === "recaptcha") {
+                grecaptcha.ready(function() {
+                    grecaptcha.execute('6LfsIrQUAAAAADX6a1sWsNVLQFKFdoA4_7N4YvdU', {action:'submit'}).then(function(token) {
+                        console.log(token);
+                        window.parent.postMessage(JSON.stringify({
+                            event_code: 'ym-client-event', data: JSON.stringify({
+                                event: {
+                                    code: "rating",
+                                    data: {
+                                        recaptcha: token
+                                    }
+                                }
+                            })
+                        }), '*');
+                        return;
+                    });
+                });
+            } 
+            else{
+                return;
+            }
+         }
+        return;
+    } catch (error) {
+        console.log(error, "error")
+        return;
+    }
+}, false);
