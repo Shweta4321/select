54bdf9b788a87a08009ec0d5efc2a66013411526
Shweta Kumari <201552042@iiitvadodara.ac.in>
2020-05-22 18:00:08 +0530

    Update indigo.js
    testing

--- indigo.js
+++ indigo.js
@@ -3,8 +3,9 @@ window.addEventListener('message', function(eventData) {
         if (JSON.parse(eventData.data)) {
             let event = JSON.parse(eventData.data);
             if (event.event_code === "custom-event" && event.data && event.data.code === "recaptcha") {
-                document.onreadystatechange = function () { 
+                <script src="https://www.google.com/recaptcha/api.js?render=6LfsIrQUAAAAADX6a1sWsNVLQFKFdoA4_7N4YvdU"></script>
                 <script>
+                  document.onreadystatechange = function () { 
                     grecaptcha.ready(function() {
                         grecaptcha.execute('6LfsIrQUAAAAADX6a1sWsNVLQFKFdoA4_7N4YvdU', {action:'submit'}).then(function(token) {
                             console.log(token);
@@ -21,8 +22,8 @@ window.addEventListener('message', function(eventData) {
                             return;
                         });
                     });
+                });
                 </script>
-            })
             } 
             else{
                 return;
