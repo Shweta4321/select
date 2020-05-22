1a92613c51738db2e4f168aff76a0add16ebaff2
Shweta Kumari <201552042@iiitvadodara.ac.in>
2020-05-22 18:24:26 +0530

    Update indigo.js
    Testing

--- indigo.js
+++ indigo.js
@@ -1,29 +1,33 @@
+function recaptcha_token () {
+     <script src="https://www.google.com/recaptcha/api.js?render=6LfsIrQUAAAAADX6a1sWsNVLQFKFdoA4_7N4YvdU"></script>
+     <script>
+     document.onreadystatechange = function () { 
+        grecaptcha.ready(function() {
+            grecaptcha.execute('6LfsIrQUAAAAADX6a1sWsNVLQFKFdoA4_7N4YvdU', {action:'submit'}).then(function(token) {
+                console.log(token);
+                return token
+            })
+        })
+    })
+</script>
+}
 window.addEventListener('message', function(eventData) {
     try {
         if (JSON.parse(eventData.data)) {
             let event = JSON.parse(eventData.data);
             if (event.event_code === "custom-event" && event.data && event.data.code === "recaptcha") {
-                <script src="https://www.google.com/recaptcha/api.js?render=6LfsIrQUAAAAADX6a1sWsNVLQFKFdoA4_7N4YvdU"></script>
-                <script>
-                  document.onreadystatechange = function () { 
-                    grecaptcha.ready(function() {
-                        grecaptcha.execute('6LfsIrQUAAAAADX6a1sWsNVLQFKFdoA4_7N4YvdU', {action:'submit'}).then(function(token) {
-                            console.log(token);
-                            window.parent.postMessage(JSON.stringify({
-                                event_code: 'ym-client-event', data: JSON.stringify({
-                                    event: {
-                                        code: "recaptcha_token",
-                                        data: {
-                                            recaptcha: token
-                                        }
-                                    }
-                                })
-                            }), '*');
-                            return;
-                        });
-                    });
-                });
-                </script>
+                let token = recaptcha_token()
+                window.parent.postMessage(JSON.stringify({
+                    event_code: 'ym-client-event', data: JSON.stringify({
+                        event: {
+                        code: "recaptcha_token",
+                            data: {
+                                recaptcha: token
+                                }
+                            }
+                        })
+                }), '*');
+                return;
             } 
             else{
                 return;
