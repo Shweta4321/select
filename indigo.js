2ec28b566948491014e507a5ece17d43e05b682c
Shweta Kumari <201552042@iiitvadodara.ac.in>
2020-05-22 17:02:48 +0530

    Update indigo.js
    testing

--- indigo.js
+++ indigo.js
@@ -1,10 +1,10 @@
+<script src="https://www.google.com/recaptcha/api.js?render=6LfsIrQUAAAAADX6a1sWsNVLQFKFdoA4_7N4YvdU"></script>
 window.addEventListener('message', function(eventData) {
     try {
         if (JSON.parse(eventData.data)) {
             let event = JSON.parse(eventData.data);
             if (event.event_code === "custom-event" && event.data && event.data.code === "recaptcha") {
-                console.log("I ma in event");
-                <script src="https://www.google.com/recaptcha/api.js?render=6LfsIrQUAAAAADX6a1sWsNVLQFKFdoA4_7N4YvdU"></script>
+                document.onreadystatechange = function () {
                 <script>
                     grecaptcha.ready(function() {
                         grecaptcha.execute('6LfsIrQUAAAAADX6a1sWsNVLQFKFdoA4_7N4YvdU', {action:'submit'}).then(function(token) {
@@ -12,7 +12,7 @@ window.addEventListener('message', function(eventData) {
                             window.parent.postMessage(JSON.stringify({
                                 event_code: 'ym-client-event', data: JSON.stringify({
                                     event: {
-                                        code: "rating",
+                                        code: "recaptcha_token",
                                         data: {
                                             recaptcha: token
                                         }
@@ -23,6 +23,7 @@ window.addEventListener('message', function(eventData) {
                         });
                     });
                 </script>
+            })
             } 
             else{
                 return;
