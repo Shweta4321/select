de405ebc60448dbf2629763bcbe6405583f830c0
Shweta Kumari <201552042@iiitvadodara.ac.in>
2020-05-26 16:58:22 +0530

    Update indigo.js
    testing

--- indigo.js
+++ indigo.js
@@ -4,13 +4,13 @@ function recaptcha_token () {
      console.log(fileref,"i am in fileref")
      let fileref1=document.createElement('script')
      console.log(fileref1, "i am here")
-     fileref1.textContent = 'function onloadCallback() {'+
-          'grecaptcha.ready(function() {'+
-               'grecaptcha.execute('6LfsIrQUAAAAADX6a1sWsNVLQFKFdoA4_7N4YvdU', {action:'submit'}).then(function(token) {'+
-                    'return token'+
-               '});'+
-          '});'+
-     '}';
+     fileref1.textContent = "function onloadCallback() {"+
+          "grecaptcha.ready(function() {"+
+               "grecaptcha.execute('6LfsIrQUAAAAADX6a1sWsNVLQFKFdoA4_7N4YvdU', {action:'submit'}).then(function(token) {"+
+                    "return token"+
+               "});"+
+          "});"+
+     "}";
      document.body.appendChild(fileref1);
      console.log(fileref1,"i am in fileref1)
 }
@@ -18,7 +18,11 @@ window.addEventListener('message', function(eventData) {
     try {
         if (JSON.parse(eventData.data)) {
             let event = JSON.parse(eventData.data);
-            if (event.event_code === "custom-event" && event.data && event.data.code === "recaptcha") {
+             if (event.event_code === "custom-event" && event.data && event.data.code === "live_agent") {
+                var newWindow = window.open(event.data.data);
+                return;
+            }
+            else if (event.event_code === "custom-event" && event.data && event.data.code === "recaptcha") {
                 let token = recaptcha_token()
                 window.parent.postMessage(JSON.stringify({
                     event_code: 'ym-client-event', data: JSON.stringify({
