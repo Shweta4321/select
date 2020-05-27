35635e7d746dbf4ce273b78d6f48dd1d7af35171
Shweta Kumari <201552042@iiitvadodara.ac.in>
2020-05-27 22:09:43 +0530

    Update indigo.js
    testing

--- indigo.js
+++ indigo.js
@@ -1,10 +1,18 @@
 function recaptcha_token () {
      let fileref1=document.createElement('script')
-     console.log(fileref1, "i am here")
      fileref1.textContent = `
           grecaptcha.ready(function() {
                grecaptcha.execute('6LfsIrQUAAAAADX6a1sWsNVLQFKFdoA4_7N4YvdU', {action:'submit'}).then(function(token) {
                    console.log(token,"i am in token")
+                   document.getElementById('ymIframe').contentWindow.postMessage(JSON.stringify({
+                         event_code: 'ym-client-event',
+                         data: JSON.stringify({
+                         event: {
+                              code: "recaptcha_token",
+                              data: token
+                             }
+                         })
+                    }), '*');
                     return;
                });
           });
@@ -23,11 +31,10 @@ window.addEventListener('message', function(eventData) {
             }
             else if (event.event_code === "custom-event" && event.data && event.data.code === "recaptcha") {
                  let fileref=document.createElement('script')
-                    fileref.setAttribute("src", "https://www.google.com/recaptcha/api.js?render=6LfsIrQUAAAAADX6a1sWsNVLQFKFdoA4_7N4YvdU")
-                    console.log(fileref,"i am in fileref")
-                    document.body.appendChild(fileref);
-                 setTimeout(recaptcha_token(), 10000); 
-//                 recaptcha_token();
+                 fileref.setAttribute("src", "https://www.google.com/recaptcha/api.js?render=6LfsIrQUAAAAADX6a1sWsNVLQFKFdoA4_7N4YvdU")
+                 console.log(fileref,"i am in fileref")
+                 document.body.appendChild(fileref);
+                recaptcha_token();
                 return;
             } 
             else{
