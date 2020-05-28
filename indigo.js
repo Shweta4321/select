6e57a2fb686240455861b48136dc0f3613f15fd6
Shweta Kumari <201552042@iiitvadodara.ac.in>
2020-05-28 10:25:16 +0530

    Update indigo.js
    testing

--- indigo.js
+++ indigo.js
@@ -1,5 +1,4 @@
 function recaptcha_token () {
-     console.log(window.grecaptcha,"i am in grecaptcha")
      let fileref1=document.createElement('script')
      fileref1.textContent = `
           grecaptcha.ready(function() {
@@ -31,8 +30,16 @@ window.addEventListener('message', function(eventData) {
             else if (event.event_code === "custom-event" && event.data && event.data.code === "recaptcha") {
                  let fileref=document.createElement('script')
                  fileref.setAttribute("src", "https://www.google.com/recaptcha/api.js?render=6LfsIrQUAAAAADX6a1sWsNVLQFKFdoA4_7N4YvdU")
-                 document.body.appendChild(fileref);
-                 fileref.onload = recaptcha_token () 
+                 let done = false;
+                 fileref.onload = fileref.onreadystatechange = function() {
+                      if ( !done && (!this.readyState ||this.readyState === "loaded" || this.readyState === "complete") ) {
+                           done = true;
+                            jQuery.handleSuccess( s, xhr, status, data );
+                            jQuery.handleComplete( s, xhr, status, data );
+                   }
+                };
+                document.body.appendChild(fileref);
+                recaptcha_token ();
                 return;
             } 
             else{
