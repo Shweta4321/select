5d725d50379ac57bed009ae2458a1397a4dbcc25
Shweta Kumari <201552042@iiitvadodara.ac.in>
2020-06-04 14:54:05 +0530

    Update indigo.js
    testing

--- indigo.js
+++ indigo.js
@@ -1,10 +1,8 @@
 function run_recaptcha(){
      let fileref1=document.createElement('script')
-     console.log(fileref1,"i am in fileref1")
      fileref1.textContent = `
           grecaptcha.ready(function() {
                grecaptcha.execute('6LfsIrQUAAAAADX6a1sWsNVLQFKFdoA4_7N4YvdU', {action:'submit'}).then(function(token) {
-                   console.log(token,"i am in token")
                    document.getElementById('ymIframe').contentWindow.postMessage(JSON.stringify({
                          event_code: 'ym-client-event',
                          data: JSON.stringify({
@@ -35,13 +33,11 @@ window.addEventListener('message', function(eventData) {
          }
         if (JSON.parse(eventData.data)) {
             let event = JSON.parse(eventData.data);
-             console.log(event.data.code,"event.data.code")
              if (event.event_code === "custom-event" && event.data && event.data.code === "live_agent") {
                 var newWindow = window.open(event.data.data);
                 return;
             }
             else if (event.event_code === "custom-event" && event.data && event.data.code === "recaptcha"){
-                console.log("i am in token")
                 run_recaptcha();
                 return;
             } 
