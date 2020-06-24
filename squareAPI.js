cb368d3eb8172780b4dfde4512b764adc36ece64
Shweta Kumari <201552042@iiitvadodara.ac.in>
2020-06-24 15:41:50 +0530

    testing

--- squareAPI.js
+++ squareAPI.js
@@ -2,6 +2,7 @@ window.addEventListener('message', function(eventData) {
     try { 
         if (JSON.parse(eventData.data)) {
             let event = JSON.parse(eventData.data);
+            console.log(event,"in event")
              if (event.event_code === "custom-event" && event.data && event.data.code === "window_cloesd") {
                  console.log("i am here")
                  window.close();
