c39b68f707484a2d734ff073bad9ae2533c342d1
Shweta Kumari <201552042@iiitvadodara.ac.in>
2020-08-04 12:14:23 +0530

    Create ShopifyEngagement.js
    testing

---
+++ ShopifyEngagement.js
@@ -0,0 +1,8 @@
+let params = window.location.search
+console.log(params)
+if(params && params.length>0)
+{
+  let redirectURl = params.substring(13,params.length)
+  console.log(redirectURL)
+  window.location.replace(redirectURl)
+}
