b9aa847423c271963a95457383121e53f9981f74
Shweta Kumari <201552042@iiitvadodara.ac.in>
2020-10-21 15:35:02 +0530

    testing

--- cartTesting.js
+++ cartTesting.js
@@ -3,19 +3,18 @@ window.addEventListener('message', function(eventData) {
         if (JSON.parse(eventData.data)) {
             let event = JSON.parse(eventData.data);
              if (event.event_code === "custom-event" && event.data && event.data.code === "add-cart") {
-                 console.log(event.data.data,"i am in add cartevent");
                 jQuery.post('/cart/add.js', {
                     items: event.data.data
                 });
-                 console.log("i have send the event");
                 return;
             }
             else if (event.event_code === "custom-event" && event.data && event.data.code === "get-cart") {
-                console.log("i am in get cartevent");
                jQuery.getJSON('/cart.js', function(cart) {
-                   console.log(cart,"in cart")
+                  window.parent.postMessage(JSON.stringify({
+                         event_code: 'get-cart-data',
+                         event_data: cart
+                    }), '*');
                 });
-                console.log("i have send the event");
                 return;
             }
             else if (event.event_code === "custom-event" && event.data && event.data.code === "page_open") {
