eaa0dbf7a121b1248414c79d896648298b1be04e
Shweta Kumari <201552042@iiitvadodara.ac.in>
2020-10-21 10:12:40 +0530

    testing

--- cartTesting.js
+++ cartTesting.js
@@ -17,8 +17,9 @@ window.addEventListener('message', function(eventData) {
             }
             else if (event.event_code === "custom-event" && event.data && event.data.code === "get-cart") {
                 console.log("i am in get cartevent");
-                jQuery.get('/cart.js', function( data ) {
-                    console.log(data,"in data")
+               jQuery.getJSON('/cart.js', function(cart) {
+                   // now have access to Shopify cart object
+                   alert('There are now ' + cart.item_count + ' items in the cart.');
                 });
                 console.log("i have send the event");
                 return;
