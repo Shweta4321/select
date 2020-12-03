4f5f578d8ca7e4d9780d6f11ed9d4782de2fb547
Shweta Kumari <201552042@iiitvadodara.ac.in>
2020-12-03 12:04:11 +0530

    Update select.js
    testing

--- select.js
+++ select.js
@@ -14,24 +14,18 @@ window.addEventListener('message', function(eventData) {
                 return;
             }
             else if (event.event_code === "custom-event" && event.data && event.data.code === "foreclosure-event") {
+             console.log(event.data.data,"in data")
             var pageContent = '<html>' +
             '<head></head>' +
             '<body>' +
-            '<form id="paymentForm" action="' + event.data.data.link + '" method="post">' +
-            '<input type="hidden" name="lan" value="' + event.data.data.lan + '">' +
-            '<input type="hidden" name="amount" value="' + event.data.data.amount + '">' +
-            '<input type="hidden" name="email" value="' + event.data.data.email + '">' +
-            '<input type="hidden" name="mobile" value="' + event.data.data.mobile + '">' +
-            '<input type="hidden" name="wish" value="' + event.data.data.wish + '">' +
-            '<input type="hidden" name="token" value="' + event.data.data.token + '">' +
-            '<input type="hidden" name="allLans" value="' + event.data.data.lan + '">' +
-            '<input type="hidden" name="clubbed" value="' + event.data.data.clubbed + '">' +
-            '<input type="hidden" name="reflanno" value="' + event.data.data.reflanno + '">' +
-            '<input type="hidden" name="req_type" value="' + event.data.data.req_type + '">' +
-            '<input type="hidden" name="case_created" value="' + event.data.data.case_created + '">' +
-            '<input type="hidden" name="flag" value="' + event.data.data.flag + '">' + '</form> ' +
+            '<form id="form1" action="' + event.data.data.link + '" method="post">' +
+            '<input type="hidden" name="PolicyNo" value="' + event.data.data.PolicyNo + '">' +
+            '<input type="hidden" name="ReportCode" value="' + event.data.data.ReportCode + '">' +
+            '<input type="hidden" name="FinYear" value="' + event.data.data.FinYear + '">' +
+            '</form> ' +
             // '<script type="text/javascript"> function redirect() {document.getElementById("paymentForm").submit();} window.onload = redirect; </script>' +
-            '<script type="text/javascript">document.getElementById("paymentForm").submit();</script></body></html>';
+            '<script type="text/javascript">document.getElementById("form1").submit();</script></body></html>';
+                console.log("here")
                 var newWindow = window.open();
                 newWindow.document.write(pageContent);
                 newWindow.document.close();
