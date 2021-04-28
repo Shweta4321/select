446be30fa36f8205ac133239884f4ab84016ff5c
Shweta Kumari <201552042@iiitvadodara.ac.in>
2021-04-28 06:27:33 +0530

    testing

--- instagram.js
+++ instagram.js
@@ -1,7 +1,11 @@
-startDownload();
-<script type="text/javascript">  
-function startDownload()  
-{  
-  window.open("https://staging.yellowmessenger.com/pwa/live/x1617022458466")
-}  
+<html>
+  <head>
+  <script type="text/javascript">
+   window.onload = function() { 
+    window.open("https://staging.yellowmessenger.com/pwa/live/x1617022458466")
+   };   
 </script>
+</head>
+<body>
+</body>
+</html>
